36a68dd52c413cd03ba0c3f14a17f9ea3d7857d1
Cedric Sumner <cedricsumner@gmail.com>
2023-10-11 11:17:47 -0400

    Add math functions, buttons + display
    Added functions that perform the basic arithmetic operations
    and a function that takes two numbers via prompt and operates

---
+++ script.js
@@ -0,0 +1,57 @@
+
+let num1;
+let operator;
+let num2;
+
+
+
+let add = ()=>{
+    let result = num1+num2
+    console.log(result)
+}
+let sub = ()=>{
+    
+    let result = num1-num2
+    console.log(result)
+}
+let div = ()=>{
+    
+    while(num2==0){
+        console.log('You can\'t divide by 0 fool!')
+        num2=prompt('Pick another number')
+    }
+
+    let result = num1/num2
+    console.log(result)
+}
+let mult = ()=>{
+
+    let result = num1*num2
+    console.log(result)
+}
+let operate = ()=>{
+    num1=prompt('first number')
+    operator=prompt('operator')
+    num2=prompt('second number')
+    num1 = +num1
+    num2 = +num2
+
+   if(operator==='+'){
+    add()
+   }
+   else if(operator==='-'){
+    sub()
+   }
+   else if(operator==='*'){
+    mult()
+   }
+   else if (operator==='/'){
+    div()
+   }
+   else {
+    console.log('invalid input')
+   }
+    
+
+
+}
